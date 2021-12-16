fb59d18edc272be40394ceda7845680ae16ba103
Rustamspl <Rustamspl@gmail.com>
2021-12-16 19:45:31 +0600

    1

--- app/src/main/java/io/github/rustamspl/hidden_calls_blocker/IncomingCallReceiver.java
+++ app/src/main/java/io/github/rustamspl/hidden_calls_blocker/IncomingCallReceiver.java
@@ -4,14 +4,16 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import android.os.Looper;
+import android.provider.CallLog;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 
 import java.lang.reflect.Method;
+import java.util.Date;
 
 public class IncomingCallReceiver extends BroadcastReceiver {
     private static Boolean waitNum=false;
-
     @Override
     public void onReceive(Context context, Intent intent) {
         if ("android.intent.action.PHONE_STATE".equals(intent.getAction())) {
@@ -55,9 +57,25 @@ public class IncomingCallReceiver extends BroadcastReceiver {
             Object iTelephony = m1.invoke(tm);
             Method m3 = iTelephony.getClass().getDeclaredMethod("endCall");
             m3.invoke(iTelephony);
+            clearLogDelayed(context);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
+    private void   clearLogDelayed(final Context context){
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                       clearLog(context);
+                    }
+                },
+                3000);
+    }
+    private void clearLog(Context context){
+        long date = new Date(System.currentTimeMillis() - 5L * 60 * 1000).getTime();
+        context.getContentResolver().delete(CallLog.Calls.CONTENT_URI,
+                CallLog.Calls.NUMBER + "='' and "+CallLog.Calls.DATE+ " > ?",
+                new String[]{""+date});
+    }
 }
--- app/src/main/java/io/github/rustamspl/hidden_calls_blocker/MainActivity.java
+++ app/src/main/java/io/github/rustamspl/hidden_calls_blocker/MainActivity.java
@@ -4,7 +4,6 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import io.github.rustamspl.hidden_calls_blocker.R;
 
 public class MainActivity extends Activity {
     private static final int PERMISSION_REQUEST_READ_PHONE_STATE = 1;
@@ -13,16 +12,71 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (checkSelfPermission(Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_DENIED || checkSelfPermission(Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_DENIED||
-                checkSelfPermission(Manifest.permission.READ_CALL_LOG ) == PackageManager.PERMISSION_DENIED) {
-            String[] permissions = {Manifest.permission.READ_PHONE_STATE, Manifest.permission.CALL_PHONE,Manifest.permission.READ_CALL_LOG};
+        if (checkSelfPermission(Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_DENIED
+                || checkSelfPermission(Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_DENIED
+                || checkSelfPermission(Manifest.permission.READ_CALL_LOG ) == PackageManager.PERMISSION_DENIED
+                || checkSelfPermission(Manifest.permission.WRITE_CALL_LOG ) == PackageManager.PERMISSION_DENIED
+        )
+        {
+            String[] permissions = {Manifest.permission.READ_PHONE_STATE, Manifest.permission.CALL_PHONE,Manifest.permission.READ_CALL_LOG,Manifest.permission.WRITE_CALL_LOG};
             requestPermissions(permissions, PERMISSION_REQUEST_READ_PHONE_STATE);
         }
 
+//        Button b=(Button) findViewById(R.id.button);
+//        b.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                //clearLog();
+//                //readLogs();
+//            }
+//        });
     }
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
 
     }
+//    private void clearLog(){
+//        long date = new Date(System.currentTimeMillis() - 1L * 24 * 3600 * 1000).getTime();
+//        getContentResolver().delete(CallLog.Calls.CONTENT_URI,
+//                CallLog.Calls.NUMBER + "='' and "+CallLog.Calls.DATE+ " > ?",
+//                new String[]{""+date});
+//    }
+//    private void readLogs(){
+//        Cursor mCur = this.getContentResolver().query(CallLog.Calls.CONTENT_URI,
+//               null, null, null, null);
+//        mCur.moveToFirst();
+//        int number = mCur.getColumnIndex(CallLog.Calls.NUMBER);
+//        int type = mCur.getColumnIndex(CallLog.Calls.TYPE);
+////        int date = mCur.getColumnIndex(CallLog.Calls.DATE);
+////        int duration = mCur.getColumnIndex(CallLog.Calls.DURATION);
+//
+//        if (mCur != null && mCur.moveToFirst() && mCur.getCount() > 0) {
+//            while (mCur.isAfterLast() == false) {
+//                String phNumber = mCur.getString(number);
+//                String callType = mCur.getString(type);
+//
+//                String dir = null;
+//                int dircode = Integer.parseInt(callType);
+//                switch (dircode) {
+//                    case CallLog.Calls.OUTGOING_TYPE:
+//                        dir = "OUTGOING";
+//                        break;
+//
+//                    case CallLog.Calls.INCOMING_TYPE:
+//                        dir = "INCOMING";
+//                        break;
+//
+//                    case CallLog.Calls.MISSED_TYPE:
+//                        dir = "MISSED";
+//                        break;
+//                }
+//
+//                Log.d("ZZZ","Num:"+phNumber+"; ct:"+dir);
+//
+//                mCur.moveToNext();
+//            }
+//        }
+//    }
+
 
 }
